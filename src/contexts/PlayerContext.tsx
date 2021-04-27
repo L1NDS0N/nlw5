0739f75ff841d438f76f89981bb68dcb580eeb3c
Lindson França <l1nds0n@hotmail.com>
2021-04-27 19:00:31 -0300

    Loop function and stylization

--- src/contexts/PlayerContext.tsx
+++ src/contexts/PlayerContext.tsx
@@ -13,9 +13,11 @@ type PlayerContextData = {
   episodeList: Episode[];
   currentEpisodeIndex: number;
   isPlaying: boolean;
+  isLooping: boolean;
   play: (episode: Episode) => void;
   playList: (list: Episode[], index: number) => void;
   togglePlay: () => void;
+  toggleLoop: () => void;
   playPrevious: () => void;
   playNext: () => void;
   setPlayingState: (state: boolean) => void;
@@ -35,6 +37,7 @@ export function PlayerContextProvider({
   const [episodeList, setEpisodeList] = useState([]);
   const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
   const [isPlaying, setIsPlaying] = useState(false);
+  const [isLooping, setIsLooping] = useState(false);
 
   function play(episode: Episode) {
     setEpisodeList([episode]);
@@ -50,6 +53,10 @@ export function PlayerContextProvider({
 
   function togglePlay() {
     setIsPlaying(!isPlaying);
+  }
+
+  function toggleLoop() {
+    setIsLooping(!isLooping);
   }
 
   function setPlayingState(state: boolean) {
@@ -85,6 +92,8 @@ export function PlayerContextProvider({
         setPlayingState,
         hasNext,
         hasPrevious,
+        isLooping,
+        toggleLoop,
       }}
     >
       {children}
